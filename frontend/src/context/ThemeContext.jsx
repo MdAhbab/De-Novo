70a465a4043d229f65c08009c6c1489755dc3581
Istiauq Ahmed Siam <istiauq.ahmed.siam@gmail.com>
2026-01-08 14:11:23 +0600

    2nd commit

--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -18,6 +18,7 @@ export const ThemeProvider = ({ children }) => {
         }
     }, [theme]);
 
+    // alskdj
     const toggleTheme = () => {
         setTheme(prev => prev === 'light' ? 'dark' : 'light');
     };
